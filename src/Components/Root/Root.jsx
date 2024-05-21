b62991c25966f91f28b35ac4e0a4759f29cce277
Siam-al-islam <siamalislam692@gmail.com>
2024-05-21 21:23:02 +0600

    first commit

---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,11 @@
+import Header from "../Header/Header";
+
+const Root = () => {
+    return (
+        <div className="max-w-[1170px] mx-auto">
+            <Header />
+        </div>
+    );
+};
+
+export default Root;
