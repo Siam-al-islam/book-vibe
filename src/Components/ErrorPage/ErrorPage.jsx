2872c4d8f45493304289f01daf90596b393a737a
Siam-al-islam <siamalislam692@gmail.com>
2024-05-23 20:27:47 +0600

    Error page updated with error data

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+    const error = useRouteError();
+
     return (
         <div>
-            <h1 className="text-3xl font-semibold text-center md:mt-48 mt-5">Oppsss!!</h1>
+            <h1 className="text-4xl font-semibold text-center md:mt-48 mt-5">{error.status}</h1>
+            <h1 className="text-2xl font-semibold text-center mt-4">{error.statusText}</h1>
             <div className="text-center mt-8">
                 <Link to="/"><button className="btn btn-wide btn-primary">Go Home</button></Link>
             </div>
